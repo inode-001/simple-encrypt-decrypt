137c55b4a8e402f4d89b177d024238af7b804afa
inode-001 <furyfish7@proton.me>
2024-04-04 06:47:25 -0500

    Added new features

--- caesarCipher.py
+++ caesarCipher.py
@@ -1,25 +1,45 @@
-key = 3
-count = 0
-unecryptedText = "CAT HELLO"
-def encrypter(key,text):
-      count = 0
-      letters='ABCDEFGHIJKLMNOPQRSTUVWXYZ '
-      encrypt = ''
-      for let in text:
-          print(f"count{count} : {let}")
-          if let in text :
-            value = letters.find(let)
-            newValue = value + key
-            if newValue > 26 or newValue == 26:
-              newValue = newValue - 26
-              encrypt+=letters[newValue]
-            else:
-                encrypt+=letters[newValue]
-          else:
-               print("test")
-               encrypt+=let
-          count+=1
-          
-    
-      return encrypt
-print(encrypter(key,unecryptedText))
+def cipher (key,message,mode):
+    translated = ''
+    SYMBOLS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?."
+    for symbol in message:
+        if symbol in SYMBOLS:
+            symbolIndex = SYMBOLS.find(symbol)
+            if mode == "encrypt":
+                newIndex = symbolIndex + key
+            elif mode == "decrypt":
+                 newIndex = symbolIndex - key
+            
+            if newIndex >= len(SYMBOLS):
+                    newIndex = newIndex - len(SYMBOLS)
+            elif newIndex < 0:
+                newIndex = newIndex + len(SYMBOLS)
+                
+                    
+            translated = translated + SYMBOLS[newIndex]
+        else:
+            translated = translated + symbol
+    return translated
+
+print("Welcome to caesar cipher".center(50,'*'),"\n\n")
+
+print("Would you like to (input either 1 or 2): \n\t 1) Encrypt message \n\t 2) Decrypt Message")
+
+option = str(input(": > "))
+
+if option == '1':
+     mode = 'encrypt'
+     message = str(input("Enter message to encrypt : "))
+     key = int(input("Enter key to use to encrypt the message : (use integers only) : "))
+     encrypted = cipher(key,message,mode)
+     print(f"\nEncrypted message is : {encrypted}")
+
+elif option == "2":
+     mode = 'decrypt'
+     message = str(input("Enter message to decrypt : "))
+     key = int(input("Enter key used to decrypt the message : (use integers only) : "))
+
+     decrypted = cipher(key,message,mode)
+     print(f"\nDecrypted message is : {decrypted}")
+
+else:
+     print("\nInvalid option input either 1 or 2")
--- reverseCipher.py
+++ reverseCipher.py
@@ -1,5 +1,26 @@
-message = 'hello world'
-def reverse(message):
-     translate = message[::-1]
-     return translate
-print(reverse(message))
+def encrypt(message):
+    translated = message[::-1]
+    return translated
+def decrypt(message):
+      translated = message[::-1]
+      return translated
+
+
+print("Welcome to reverse cipher".center(50,'*'),"\n\n")
+
+print("Would you like to (input either 1 or 2): \n\t 1) Encrypt message \n\t 2) Decrypt Message")
+
+option = str(input(": > "))
+
+if option == '1':
+     message = input("Enter message to encrypt : ")
+     encrypted = encrypt(message)
+     print(f"\nEncrypted message is : {encrypted}")
+
+elif option == "2":
+     message = input("Enter message to decrypt : ")
+     decrypted = decrypt(message)
+     print(f"\nDecrypted message is : {decrypted}")
+
+else:
+     print("\nInvalid option input either 1 or 2")
